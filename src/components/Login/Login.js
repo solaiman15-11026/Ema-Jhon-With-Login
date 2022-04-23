2af9fb47299985bb9ef1c042b0ec09285309a709
solaiman15-11026 <samimunna49@gmail.com>
2022-04-23 09:51:04 +0600

    new

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../Firebase.init';
 import './Login.css'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const [
+        signInWithEmailAndPassword, user, error, loading] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmaill = e => {
+        setEmail(e.target.value);
+    }
+
+    if (user) {
+        navigate('/shop')
+    }
+
+    const handlePasswordd = e => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password)
+
+    }
+
     return (
         <div className='From-container'>
             <div>
                 <h2 className='font-tittle'> Login</h2>
                 <div className="input-group">
-                    <form >
+                    <form onSubmit={handleSubmit}>
                         <label htmlFor="email"><b>Email:</b>  </label> <br />
-                        <input type="email" placeholder='' /> <br /> <br />
+                        <input onBlur={handleEmaill} type="email" placeholder='' required /> <br /> <br />
                         <label htmlFor="password"><b>Password:</b>  </label> <br />
-                        <input type="password" placeholder='' />
+                        <input onBlur={handlePasswordd} type="password" placeholder='' required />
                         <br /> <br />
+
+                        <p>{error.message}</p>
+                        {
+                            loading && <p>Loading...</p>
+                        }
+
                         <input className='rrrr' type="submit" value="Login" />
                     </form>
                     <p>
